ddda50e748da61057de024595de13e633fb49755
milesaylward <miles.aylward@gmail.com>
2017-07-11 02:32:12 -0400

    update app

--- bundle.js
+++ bundle.js
@@ -3,24 +3,33 @@
 var app = angular.module('app', []);
 
 app.controller('appController', function appController($scope) {
-  $scope.num = null;
   $scope.percents = [];
+  $scope.error = '';
 
-  $scope.submit = function () {
-    if ($scope.num) {
-      var num = Math.round($scope.num * 100);
-      $scope.percents.push(num);
-
+  $scope.submit = function (num) {
+    $scope.error = '';
+    if (!isNaN(num) && num <= 1 && num >= 0) {
+      var percent = Math.round(num * 100);
+      console.log(percent);
+      $scope.percents.push(percent);
+      $scope.num = null;
+    } else if (isNaN($scope.num)) {
+      $scope.num = null;
+      $scope.error = 'You must enter a number!';
+    } else {
+      $scope.error = 'Your number must be between 0 and 1!';
       $scope.num = null;
     }
   };
 
   $scope.random = function () {
+    $scope.error = '';
     var rand = Math.round(Math.random() * 100);
     $scope.percents.push(rand);
   };
 
   $scope.clear = function () {
     $scope.percents = [];
+    $scope.error = '';
   };
 });
